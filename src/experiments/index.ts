3e9cdfc28c52cdbfec9beb340e35b73f87713fd8
Sourendas <141188069+Sourendas@users.noreply.github.com>
2026-09-03 16:43:48 +0530

    Export A/B testing public API

---
+++ src/experiments/index.ts
@@ -0,0 +1,11 @@
+export { EXPERIMENTS, HERO_CTA_COPY } from './config';
+export { useExperiment } from './useExperiment';
+export { AbDebugPanel } from './AbDebugPanel';
+export {
+  assignExperiment,
+  trackAbEvent,
+  getAssignments,
+  getVisitorId,
+  getAbEvents,
+  clearAbState,
+} from './abtest';
